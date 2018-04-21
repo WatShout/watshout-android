46aa2adc7cb4736cbefdfc0b82d2574313aca241
Ethan Houston <ethan.houston@gmail.com>
2018-04-20 18:11:16 -0700

    Commented the code

--- app/src/main/java/com/watshout/face/MainActivity.java
+++ app/src/main/java/com/watshout/face/MainActivity.java
@@ -10,6 +10,7 @@ import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -40,15 +41,36 @@ import okhttp3.Response;
 
 import static com.google.android.gms.maps.model.BitmapDescriptorFactory.fromResource;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
+/*
+
+===================================
+Welcome to the GPS app source code!
+
+I'm going to try to document this as
+well as I can, because it's very easy
+to get lost.
+
+The main thing to understand is the
+implemented methods for various methods
+like the maps and Firebase things, etc.
+
+====================================
+
+ */
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    // Globally-defined
     LocationListener locationListener;
+    LocationManager locationManager;
     DatabaseReference databaseReference;
     GoogleMap googleMapGlobal;
 
     // Identifies fine location permission
     private static final int ACCESS_FINE_LOCATION = 1;
 
+    // Gets a unique hardware ID for a device
     @SuppressLint("HardwareIds")
     private String getID() {
         return Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
@@ -57,19 +79,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_main);
 
+        // Removes the top bar on top of the map
+        getSupportActionBar().hide();
+
+        // It will help to look at the Firebase DB. This gets a reference to the
+        // 'coords' directory I've made
         databaseReference = FirebaseDatabase.getInstance().getReference().child("coords");
 
-
+        // Defines a 'fragment' of the activity dedicated to the map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+        // Honestly I copied this from StackOverflow. It gets the GPS permissions. Don't mess with
+        // it! :D
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -89,36 +117,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         // Defines a location service
-        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        locationManager = (LocationManager) getApplicationContext()
+                .getSystemService(LOCATION_SERVICE);
 
         // See below LocationListener class
         locationListener = new MyLocationListener();
 
+        // This listens for any 'change' in the child that's been selected ('coords')
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
+                // This takes the data from the database and converts it into a Java object
                 HashMap data = (HashMap) dataSnapshot.getValue();
 
+                // Gets individual values from the HashMap
                 assert data != null;
                 double lat = (double) data.get("lat");
                 double lon = (double) data.get("long");
                 double time = (double) data.get("time");
 
+                // Gets the 8th number in the timestamp. This is just for testing now
                 String stringTime = Double.toString(time);
-
                 char timeChar = stringTime.charAt(8);
-
                 int timeInt = (int) timeChar;
 
+                // Image file
                 BitmapDescriptor ic;
 
-                if(timeInt % 2 == 0){
+                // Again, just testing. Places a different marker depending on the time
+                if (timeInt % 2 == 0) {
                     ic = fromResource(R.drawable.beachflag);
                 } else {
                     ic = fromResource(R.drawable.blueflag);
                 }
 
+                // Adds a new marker on the LOCAL map. (The one on the website is written elsewhere).
                 Marker newMarker = googleMapGlobal.addMarker(new MarkerOptions()
                         .position(new LatLng(lat, lon))
                         .icon(ic));
@@ -133,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
 
+                // Testing again. If children are removed, take everything off of the map
                 googleMapGlobal.clear();
 
             }
@@ -148,18 +183,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         };
 
+        // Attaches the above listener to the DB reference
         databaseReference.addChildEventListener(childEventListener);
 
         // Unsure which minTime and minDistance values work best
+        // minTime is in milliseconds, distance in meters
         assert locationManager != null;
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager
+                .GPS_PROVIDER, 1000, 0, locationListener);
 
 
     }
 
+    // This is from StackOverflow too
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case ACCESS_FINE_LOCATION: {
 
@@ -177,21 +217,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    // This runs as the map rendering is completed
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        // IMPORTANT
+        // This ensures that we can make changes to the map outside of this function
+        // which is why we defined it globally
         googleMapGlobal = googleMap;
 
+        // This sets the starting position/zoom level
         LatLng latLng = new LatLng(-117, 30);
         float zoom = 1;
 
+        // This sets the initial view of the map
         googleMapGlobal.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
     }
 }
 
+// This class deals with POSTing the data to Firebase
 class PostData extends AsyncTask<String, Void, Void> {
 
+    // Init. client and data type
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private OkHttpClient client = new OkHttpClient();
 
@@ -249,7 +297,7 @@ class MyLocationListener implements LocationListener {
                 location.getLongitude(), location.getLatitude()
         );
 
-        // Parse data, then POST
+        // Parse data, then POST using PostData class
         String data = parseGPSData(location);
         PostData post = new PostData();
         post.execute(data);
