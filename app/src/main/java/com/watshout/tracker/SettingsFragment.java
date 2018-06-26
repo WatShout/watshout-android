cc0c27c25ff7804b2c5a16048c7a04bc7634728d
vrjmath <vrjmath@gmail.com>
2018-06-25 18:36:24 -0700

    Drawer Completed (Map Works) This is the revised version

---
+++ app/src/main/java/com/watshout/tracker/SettingsFragment.java
@@ -0,0 +1,277 @@
+package com.watshout.tracker;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+import java.io.ByteArrayOutputStream;
+
+public class SettingsFragment extends android.app.Fragment implements IPickResult {
+    final long TEN_MEGABYTE = 10 * 1024 * 1024;
+
+    FirebaseUser thisUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    String uid = thisUser.getUid();
+    String email = thisUser.getEmail();
+    String name = thisUser.getDisplayName();
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReference();
+
+    DatabaseReference ref = FirebaseDatabase
+            .getInstance()
+            .getReference();
+
+
+    ImageView mProfile;
+    EditText mEmail;
+    EditText mAge;
+    Button mEmailButton;
+    Button mAgeButton;
+
+    String fileFormat;
+    String fileName;
+
+    final int MY_PERMISSIONS_REQUEST_CAMERA = 505;
+    String[] permissions = {android.Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mProfile = view.findViewById(R.id.profile);
+        mEmail = view.findViewById(R.id.email);
+        mAge = view.findViewById(R.id.age);
+        mEmailButton = view.findViewById(R.id.email_button);
+        mAgeButton = view.findViewById(R.id.age_button);
+
+
+        checkCameraPermissions();
+
+        ref.child("users").child(uid).child("age").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mAge.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //PickImageDialog.build(new PickSetup()).show(getActivity());
+
+            }
+        });
+
+        ref.child("users").child(uid).child("profile_pic_format").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.getValue() != null){
+                    fileFormat = dataSnapshot.getValue().toString();
+                    fileName = "profile." + fileFormat;
+
+                    storageReference.child("users").child(uid).child(fileName).getBytes(TEN_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            mProfile.setImageBitmap(bmp);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle any errors
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mAgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mAge.getText().toString().length() > 0){
+
+                    int newAge = Integer.parseInt(mAge.getText().toString());
+                    ref.child("users").child(uid).child("age").setValue(newAge);
+
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                    mAge.clearFocus();
+                    mAge.setCursorVisible(false);
+
+                }
+            }
+        });
+    }
+
+    byte[] getImageData(Bitmap bmp) {
+
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, bao); // bmp is bitmap from user image file
+        bmp.recycle();
+        byte[] byteArray = bao.toByteArray();
+
+        return byteArray;
+    }
+
+    @Override
+    public void onPickResult(PickResult r) {
+        if (r.getError() == null) {
+
+            Bitmap bmp1 = r.getBitmap();
+            Bitmap bmp2 = bmp1.copy(bmp1.getConfig(), true);
+
+            mProfile.setImageBitmap(bmp1);
+
+            final byte[] data = getImageData(bmp2);
+
+            if (fileName != null){
+                storageReference.child("users").child(uid).child(fileName)
+                        .delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        storageReference.child("users").child(uid).child("profile.png")
+                                .putBytes(data).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                                Log.d("IMG", "Done uploading!");
+
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                                ref.child("users").child(uid).child("profile_pic_format").setValue("png");
+
+                            }
+                        });
+
+                    }
+                });
+            } else {
+                storageReference.child("users").child(uid).child("profile.png")
+                        .putBytes(data).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        Log.d("IMG", "Done uploading!");
+
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        ref.child("users").child(uid).child("profile_pic_format").setValue("png");
+
+                    }
+                });
+            }
+
+
+        } else {
+            Log.d("IMG", "This didn't work: " + r.getError().toString());
+        }
+    }
+
+    public void checkCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this.getActivity(),
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this.getActivity(),
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    permissions,
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Open
+                    // camera, take photo.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+        }
+    }
+}
