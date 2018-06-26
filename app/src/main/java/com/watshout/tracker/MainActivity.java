fa3e1adff80e959a56e16721c6d5d2635184c441
vrjmath <vrjmath@gmail.com>
2018-06-25 18:49:39 -0700

    Drawer Completed (Map Works) This is the revised version

--- app/src/main/java/com/watshout/tracker/MainActivity.java
+++ app/src/main/java/com/watshout/tracker/MainActivity.java
@@ -117,6 +117,10 @@ public class MainActivity extends AppCompatActivity implements
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.screen_area, new MapFragment())
+                .commit();
 
     }
 
