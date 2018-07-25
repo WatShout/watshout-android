d2fce92d48460283f19d8da9dce8c005279101b1
Ethan Houston <ethan.houston@gmail.com>
2018-07-24 23:02:25 -0700

    Added splash screen

--- app/src/main/java/com/watshout/watshout/MainActivity.java
+++ app/src/main/java/com/watshout/watshout/MainActivity.java
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setBackgroundColor(0xFF262523);
 
         final View navView = navigationView.inflateHeaderView(R.layout.nav_header);
         TextView mEmail = navView.findViewById(R.id.nav_head_email);
