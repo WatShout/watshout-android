b36c4fdce159cf65b9961395ed655c67b9876371
Ethan Houston <ethan.houston@gmail.com>
2018-07-30 21:50:30 -0700

    Splash screen should work now

--- app/src/main/java/com/watshout/watshout/SplashActivity.java
+++ app/src/main/java/com/watshout/watshout/SplashActivity.java
@@ -4,22 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class SplashActivity extends AppCompatActivity {
-
-    FirebaseUser thisUser = FirebaseAuth.getInstance().getCurrentUser();
-
-    String uid = thisUser.getUid();
-    String email = thisUser.getEmail();
-    String name = thisUser.getDisplayName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
         finish();
     }
