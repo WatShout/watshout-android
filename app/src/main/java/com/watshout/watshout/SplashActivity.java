69a50bc5b62f73981bc937e428ae6526463bc10a
Ethan Houston <ethan.houston@gmail.com>
2018-07-30 21:33:50 -0700

    started on splash

--- app/src/main/java/com/watshout/watshout/SplashActivity.java
+++ app/src/main/java/com/watshout/watshout/SplashActivity.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+
+    FirebaseUser thisUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    String uid = thisUser.getUid();
+    String email = thisUser.getEmail();
+    String name = thisUser.getDisplayName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
